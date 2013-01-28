fb2ae9db5cd0b5e87d525e2eddb28e3093580fae
javier garcia <javier@idraw.es>
2013-01-28 19:20:17 +0100

    Initial Commit

---
+++ Dictionary/IXViewController.h
@@ -0,0 +1,13 @@
+//
+//  IXViewController.h
+//  Dictionary
+//
+//  Created by Javier García Gallego on 28/01/13.
+//  Copyright (c) 2013 ixavy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IXViewController : UIViewController
+
+@end
